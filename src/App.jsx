c4b76d2ac16cdba102d57190f6e9512c7efe15ac
John Angel Piraban Castellanos <jhoanpc85@gmail.com>
2024-08-01 14:25:51 -0500

    add component person

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import HomePage from './app/home/HomePage'
 const App = () => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hola App</p>
+      <HomePage></HomePage>
     </div>
   )
 }
---
+++ src/app/home/HomePage.jsx
@@ -0,0 +1,13 @@
+
+import Person from "../credit/Person"
+const HomePage = () => {
+  return (
+    <div>
+        <p>Hola Home Page</p>
+      <Person></Person>
+    </div>
+  )
+}
+
+export default HomePage
+
